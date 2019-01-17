1f1414328f8fc50470ba478deddfde47052ab957
Joshua Wood <jwood14@apu.edu>
2019-01-16 18:01:15 -0800

    Added Firebase as a project dependency and added Firebase api keys to Appjs

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import firebase from 'firebase';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDCp3X_njrJy-_RJYTzUOvWZQrPYdekBak",
+      authDomain: "when-and-where-c8a71.firebaseapp.com",
+      databaseURL: "https://when-and-where-c8a71.firebaseio.com",
+      projectId: "when-and-where-c8a71",
+      storageBucket: "when-and-where-c8a71.appspot.com",
+      messagingSenderId: "521022677241"
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
