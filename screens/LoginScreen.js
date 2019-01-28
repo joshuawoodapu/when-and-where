525574c6f43989d58d42401c91bf7c7833a7e8f3
Mattaniah Aytenfsu <maytenfsu14@apu.edu>
2019-01-27 16:10:14 -0800

    restructing login, adding form error msg, saving email/password input

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,21 +1,87 @@
 import React, { Component } from 'react';
 import { View, Image, Text, TextInput, Button, Icon, Container, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import firebase from 'firebase';
 import LoginButton from '../components/loginComponents/LoginButton';
-import LoginForm from '../components/loginComponents/LoginForm';
+// import LoginForm from '../components/loginComponents/LoginForm';
+import Spinner from "../components/common/Spinner"
 import LoginRedirect from '../components/loginComponents/LoginRedirect';
 
 class LoginScreen extends Component {
+    state = { email: '', password: '', error: '', loading: false };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        this.setState({ error: '', loading: true });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
+            });
+    }
+
+    onLoginFail() {
+        this.setState({ error: 'Authentication Failed.', loading: false });
+    }
+
+    onLoginSuccess() {
+        this.setState({ 
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+        return (
+            <Button 
+                onPress={this.onButtonPress.bind(this)}
+                title="Log in" 
+            />
+        );
+    }
+
     render() {
         return (
             <DismissKeyboard style={styles.toplevel}>
                 <View style={styles.mainContainer}>
                     <Image style={styles.logo}
                     source={require('../components/images/whenwherelogo.png')}/>
-                    <View>
-                        <LoginForm />
-                        <LoginRedirect style={styles.redirect}/>
+
+                    <View style={styles.formStyle}>
+                        <TextInput 
+                            placeholder="Email"
+                            label="Email"
+                            value={this.state.email}
+                            onChangeText={email => this.setState({ email })}
+                            returnKeyType="next"
+                            style={styles.input1}
+                        />
+                        <TextInput 
+                            secureTextEntry
+                            placeholder="Password"
+                            label="Password"
+                            value={this.state.password}
+                            onChangeText={password => this.setState({ password })}
+                            returnKeyType="next"
+                            style={styles.input2}
+                        />
                     </View>
-                    <LoginButton style={styles.button}/>
+            
+                    <Text style={styles.errorTextStyle}>
+                        {this.state.error}
+                    </Text>
+                    <LoginRedirect style={styles.redirect}/>
+
+
+                    {this.renderButton()}
+                    
                 </View>
             </DismissKeyboard>
         )
@@ -61,9 +127,36 @@ const styles = StyleSheet.create({
     testBorder: {
         borderColor: '#0ac45555',
         borderWidth: 2 
+    },
+    formStyle: {
+        // padding: 35,
+        justifyContent: 'space-between',
+    },
+    input2: {
+        height: 50,
+        backgroundColor: '#ffffff',
+        //marginBottom: 25,
+        borderWidth: 2,
+        borderColor: '#B8BeC1',
+        borderRadius: 15,
+        color: '#B8BeC1',
+        paddingHorizontal: 10,
+    },
+    input1: {
+        height: 50,
+        backgroundColor: '#ffffff',
+        marginBottom: 25,
+        borderWidth: 2,
+        borderColor: '#B8BeC1',
+        borderRadius: 15,
+        color: '#B8BeC1',
+        paddingHorizontal: 10,
+    },
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
-  
-
 });
 
 export default LoginScreen;
