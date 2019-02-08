af4d373ae12c098b855cb3b688d158571024ef1f
Mattaniah Aytenfsu <maytenfsu14@apu.edu>
2019-02-08 00:24:17 -0800

    added some text styling

--- src/screens/Comment.js
+++ src/screens/Comment.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import { Divider } from 'react-native-elements';
 
 class Comment extends Component {
     
     render() {
         return (
             <View>
-                <Text>{this.props.username}</Text>
-                <Text>{this.props.time}</Text>
-                <Text>{this.props.content}</Text>
+                <Text style={styles.username}>{this.props.username}</Text>
+                <Text style={styles.content}>{this.props.time}</Text>
                 
-                <Text>-------</Text>
+                <Text style={styles.content}>{this.props.content}</Text>
+                
+                
+                <Divider style={{ backgroundColor: '#B0CAED' }} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    username: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#2661B2'
+    },
+    content: {
+        fontSize: 15,
+        fontWeight: '400',
+        color: '#2661B2'
+    },
+});
 
 export default Comment;
