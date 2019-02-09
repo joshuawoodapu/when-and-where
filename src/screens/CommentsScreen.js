6f78494667f3dcd2adc68674c8ffcc940a0e2418
Mattaniah Aytenfsu <maytenfsu14@apu.edu>
2019-02-09 13:16:08 -0800

    put comments in a scrollable view

--- src/screens/CommentsScreen.js
+++ src/screens/CommentsScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import Comment from './Comment';
+import { View, ScrollView, Text, FlatList, SectionList } from 'react-native';
+import Comment from '../components/commentComponents/Comment';
 import { ListItem } from 'react-native-elements';
 
 class CommentsScreen extends Component {
@@ -13,19 +13,10 @@ class CommentsScreen extends Component {
         }
     };
 
-    /* <FlatList
-        data={comments}
-        renderItem={({ comment, index }) =>
-            <Comment
-                username={comment.user}
-                content={comment.comment}
-                time={comment.created} />}
-        /> 
-    */
-
     render() {
+
         return (
-            <View>
+            <ScrollView>
                 {comments.map((c, index) =>
                     <View key={index}>
                         <Comment
@@ -35,7 +26,7 @@ class CommentsScreen extends Component {
                             created={c.created} />
                     </View>
                 )}
-            </View>
+            </ScrollView>
         )
     }
 }
@@ -51,13 +42,37 @@ const comments = [
         user: 'Janelle M.',
         avatar_url: 'https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg',
         content: "It be like that sometimes",
-        created: '1h'
+        created: '2h'
     },
     {
         user: 'Emily H.',
         avatar_url: 'https://tinyfac.es/data/avatars/03F55412-DE8A-4F83-AAA6-D67EE5CE48DA-200w.jpeg',
         content: "Would frozen yogurt be okay?",
-        created: '5m'
-    }
+        created: '1h'
+    },
+    {
+        user: 'Joseph K.',
+        avatar_url: 'https://tinyfac.es/data/avatars/26CFEFB3-21C8-49FC-8C19-8E6A62B6D2E0-200w.jpeg',
+        content: "That's still dairy",
+        created: '1h'
+    },
+    {
+        user: 'Emily H.',
+        avatar_url: 'https://tinyfac.es/data/avatars/03F55412-DE8A-4F83-AAA6-D67EE5CE48DA-200w.jpeg',
+        content: "Not all frozen yogurt",
+        created: '30m'
+    },
+    {
+        user: 'Janelle M.',
+        avatar_url: 'https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg',
+        content: "But most of them are",
+        created: '25m'
+    },
+    {
+        user: 'Joseph K.',
+        avatar_url: 'https://tinyfac.es/data/avatars/26CFEFB3-21C8-49FC-8C19-8E6A62B6D2E0-200w.jpeg',
+        content: "okay",
+        created: '1m'
+    },
 ]
 export default CommentsScreen;
