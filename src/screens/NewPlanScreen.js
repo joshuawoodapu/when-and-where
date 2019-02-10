380fc38f881f1edcb6136796e3df875c00a0f211
Mattaniah Aytenfsu <maytenfsu14@apu.edu>
2019-02-09 17:47:10 -0800

    aligned close button on modal

--- src/screens/NewPlanScreen.js
+++ src/screens/NewPlanScreen.js
@@ -5,45 +5,57 @@ import PieChart from 'react-native-pie-chart';
 import { Icon } from 'react-native-elements';
 
 class NewPlanScreen extends Component {
-
-    state = {
-        visibleVotingModal: false,
-        renderedActivityGroup: null
-    };     
-    
-    /* should take in an activitySlot as a param */
-    renderPieChart = (voteNums) => (
-        <View style={voteResults.modalContent}>
-            <Icon name='clear' color='#2661B2' onPress={ this._toggleVotingModal } />
-            <Text style={voteResults.header}> Voting Results </Text>
-            <PieChart
-                style={voteResults.chart}
-                chart_wh={225}
-                series={voteNums}
-                sliceColor={sliceColors}
-            />
-
-            <View style={voteResults.votingLegendContainer}>
-                {activityGroup1.map((activity, index) => 
-                    <View key={activity.activityName}>
-                        <View style={{width: 20, height: 20, borderRadius: 100/2, backgroundColor: sliceColors[index]}} />
-                        <Text style={voteResults.actName}>{activity.activityName}</Text>
-                    </View>
-                )}
-            </View>
-
-        </View>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibleVotingModal: true,
+            renderedActivityGroup: null
+        }
+    }
 
     _toggleVotingModal = () =>
         this.setState({ visibleVotingModal: !this.state.visibleVotingModal });
-        
-        
-    render() {
 
+    /* should take in an activitySlot as a param */
+    renderPieChart = (voteNums) => (
+        <View style={styles.container}>
+            <View style={styles.modalContent}>
+
+                <View style={styles.closeButton}>
+                    <Icon
+                        name='clear'
+                        color='#2661B2'
+                        size={30}
+                        onPress={this._toggleVotingModal}
+                    />
+                </View>
+
+                <View style={styles.centered}>
+                    <Text style={styles.header}> Voting Results </Text>
+                    <PieChart
+                        style={{ marginBottom: 25 }}
+                        chart_wh={225}
+                        series={voteNums}
+                        sliceColor={sliceColors}
+                    />
+                </View>
+
+                <View style={styles.votingLegendContainer}>
+                    {activityGroup1.map((activity, index) =>
+                        <View style={styles.keyContainer} key={activity.activityName}>
+                            <View style={{ width: 20, height: 20, borderRadius: 100 / 2, backgroundColor: sliceColors[index], marginRight: 7 }} />
+                            <Text style={styles.actName}>{activity.activityName}</Text>
+                        </View>
+                    )}
+                </View>
+            </View>
+        </View>
+    );
+
+    render() {
         // TODO: need to put this somewhere else
         let voteNums = [];
-        activityGroup1.map( (activity) => {
+        activityGroup1.map((activity) => {
             voteNums.push(activity.numVotes);
         });
 
@@ -51,7 +63,7 @@ class NewPlanScreen extends Component {
             <View>
                 <Text>NewPlan!!!!</Text>
                 <Text>{"\n\n\n"}</Text>
-                
+
                 <TouchableOpacity onPress={this._toggleVotingModal}>
                     <Text>Activity 1</Text>
                 </TouchableOpacity>
@@ -64,59 +76,71 @@ class NewPlanScreen extends Component {
     }
 }
 
-let activityGroup1 = [
-    { 
-      activityName: 'The Broad',
-      numVotes: 5 
-    }, 
-    { 
-      activityName: 'LACMA',
-      numVotes: 1
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
     },
-    { 
-      activityName: 'California Science Center',
-      numVotes: 3
-    },
-    { 
-      activityName: 'The Getty',
-      numVotes: 8
-    }
-];
-
-const sliceColors = ['#F7D055','#F387B8','#6A6789', '#A0C1ED', '#EC7248'];
-
-const voteResults = StyleSheet.create({
     modalContent: {
         backgroundColor: "white",
-        padding: 22,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 10,
+        padding: 18,
+        borderRadius: 20,
         borderColor: "rgba(0, 0, 0, 0.1)"
     },
-    chart: {
-        marginBottom: 25
+    centered: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    closeButton: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     },
     header: {
         fontSize: 30,
         color: '#413C77',
         fontWeight: 'bold',
-        marginBottom: 25
+        marginBottom: 20
     },
     actName: {
         fontSize: 15,
         fontWeight: '600',
-        color: '#2661B2'
+        color: '#2661B2',
+    },
+    keyContainer: {
+        flexDirection: 'row',
+        paddingVertical: 8
     },
     votingLegendContainer: {
-        // flex: 1,
-        // flexDirection: 'row',
-        // flexWrap: 'wrap',
-        // alignItems: 'flex-start' 
+        justifyContent: 'center', 
+        alignItems: 'flex-start', 
+        flexDirection: 'row',
+        flexWrap: 'wrap'
     },
-    // votingLegendItem: {
-    //     width: '50%',
-    // }
 });
+
+let activityGroup1 = [
+    {
+        activityName: 'The Broad',
+        numVotes: 5
+    },
+    {
+        activityName: 'LACMA',
+        numVotes: 3
+    },
+    {
+        activityName: 'California Science Center',
+        numVotes: 4
+    },
+    {
+        activityName: 'The Getty',
+        numVotes: 7
+    },
+    {
+        activityName: 'MOCA',
+        numVotes: 1
+    }
+];
+
+const sliceColors = ['#F7D055', '#F387B8', '#6A6789', '#A0C1ED', '#EC7248'];
+
 
 export default NewPlanScreen;
