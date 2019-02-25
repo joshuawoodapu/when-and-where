a5a414672a1e5af15dcc44b3bae7394989ff762a
rjacoby15 <rjacoby15@apu.edu>
2019-02-24 20:24:05 -0800

    Finished styling on Invite Collab screen, added modals to plan options screen

--- src/screens/InviteCollabScreen.js
+++ src/screens/InviteCollabScreen.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity, FlatList } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import Avatars from '../../src/components/common/Avatars';
+import DynamicInput from '../components/common/DynamicInput';
 
 export default class InviteCollabsScreen extends Component {
+    static navigationOptions = {
+        title: 'INVITE COLLABORATORS',
+        headerTitleStyle: {
+            color: '#2661B2',
+            fontSize: 14,
+            fontWeight: 'bold'
+        }
+    };
     render() {
+
+        var avatars = [{name: "Kasey B.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
+        {name: "Caroline R.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'},
+        {name: "Hannah M.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'},
+        {name: "Steve R.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'},
+        {name: "Elliot S.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
+        {name: "Hannah K.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},
+        {name: "Matt A.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'},
+        {name: "Sabine C.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'},
+        {name: "Darnell D.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'},
+        {name: "Olivia C.", size: "medium", rounded: true, uri:'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'},];
+
         return (
            <View>
-                <View style={styles.headerStyle}>
-                    <Text style={styles.headerTextStyle}>INVITE COLLABORATORS</Text>
-                </View> 
 
                 <View style={styles.textHeaderView}>
                     <Text style={styles.textHeader}>ADD USERS</Text>
@@ -17,13 +35,36 @@ export default class InviteCollabsScreen extends Component {
 
                 <DismissKeyboard>
                     <View style={styles.formStyle}>
-                        <TextInput placeholder="Name or Username" style={styles.input}/>
+                    <DynamicInput placeholderList={[
+                      {placeholder: 'Name or username',
+                        inputContainerStyle: 'regScreenInput',
+                        autoCorrect: false,
+                        autoCapitalize: "words",
+                        stateLabel: "name",
+                        onChange: this.handleNameChange,
+                        returnKeyType: 'done'} ]} />
                     </View>
                 </DismissKeyboard>
 
-                <Avatars />
+                <View style={styles.avatarsStyle}>
+                  <FlatList
+                      data={avatars}
+                      horizontal={true}
+                      showsHorizontalScrollIndicator={false}
+                      renderItem={({item}) =>
+                        <Avatars
+                            size={item.size}
+                            rounded={item.rounded}
+                            uri={item.uri}
+                            avatarContainer="newPlanContainer"
+                            name={item.name}
+                        />
+                      }
+                      keyExtractor={(item, index) => index.toString()}
+                  />
+                </View>
 
-                <View style={styles.textHeaderView}>
+                <View style={styles.inviteMessageView}>
                     <Text style={styles.textHeader}>INVITATION MESSAGE</Text>
                 </View>
 
@@ -63,12 +104,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        marginLeft: 40
+        marginLeft: 40,
+        marginTop: 50,
     },
     textHeader: {
         fontSize: 12,
         color: '#2661B2',
         fontWeight: 'bold',
+    },
+    inviteMessageView: {
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        marginLeft: 40,
     },
 /////////////Text Input Box///////////////////
     formStyle: {
@@ -92,13 +140,12 @@ const styles = StyleSheet.create({
     },
     messageBox: {
         height: 160,
-        backgroundColor: '#ffffff',
-        marginBottom: 25,
-        borderWidth: 2,
+        borderWidth: 1,
         borderColor: '#B8BeC1',
         borderRadius: 15,
-        color: '#B8BeC1',
-        paddingHorizontal: 10,
+        paddingTop: 16,
+        paddingBottom: 14,
+        paddingHorizontal: 20,
     },
 /////////////Invite Button//////////////////////////
     inviteContainer: {
@@ -108,6 +155,7 @@ const styles = StyleSheet.create({
         height: 60,
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: 20
     },
     inviteButtonText: {
         textAlign: 'center',
@@ -119,6 +167,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-evenly',
     },
+    //////////////////////Scroll View Styling///////////////////
+    avatarsStyle: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginRight: 20,
+        marginLeft: 20
+    }
 });
 
 const DismissKeyboard = ({children}) => (
--- src/screens/PlanOptionsScreen.js
+++ src/screens/PlanOptionsScreen.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
-import  FlipToggle  from 'react-native-flip-toggle-button';
+import FlipToggle from 'react-native-flip-toggle-button';
+import Modal from "react-native-modal";
+import Dropdown from '../components/common/Dropdown';
 
 export default class PlanOptionsScreen extends Component {
+
+    static navigationOptions = {
+        title: 'PLAN OPTIONS',
+        headerTitleStyle: {
+            color: '#2661B2',
+            fontSize: 14,
+            fontWeight: 'bold'
+        }
+    };
 
     constructor(props) {
         super(props);
         this.state = {
-          isSwitch1On: false,}
-        };
+            isSwitch1On: false,
+            visibleNotificationModal: false,
+            visibleDatesModal: false,
+        }
+    };
 
     onCollabPress() {
         this.props.navigation.navigate('CollabInvite');
     }
 
+    toggleNotificationModal = () => this.setState({ visibleNotificationModal: !this.state.visibleNotificationModal });
+    toggleDatesModal = () => this.setState({ visibleDatesModal: !this.state.visibleDatesModal });
+
     render() {
+
         return (
-           <View>
-                <View style={styles.headerStyle}>
-                    <Text style={styles.headerTextStyle}>PLAN OPTIONS</Text>
-                </View>
+            <View>
 
                 <View style={styles.privacyOptionContainer}>
                     <View style={styles.organizeView}>
@@ -41,13 +56,13 @@ export default class PlanOptionsScreen extends Component {
                             buttonOnColor={'#Ed7248'}
                             sliderOnColor={'#fff'}
                             onToggle={(value) => {
-                            this.setState({ isSwitch1On: value });
+                                this.setState({ isSwitch1On: value });
                             }}
                         />
                     </View>
                 </View>
 
-                <TouchableOpacity style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.toggleNotificationModal}>
                     <View style={styles.organizeView}>
                         <Icon
                             name='notifications'
@@ -69,7 +84,7 @@ export default class PlanOptionsScreen extends Component {
                     </View>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.toggleDatesModal}>
                     <View style={styles.organizeView}>
                         <Icon
                             name='date-range'
@@ -86,7 +101,35 @@ export default class PlanOptionsScreen extends Component {
                     </TouchableOpacity>
                 </View>
 
-            </View> 
+                <Modal isVisible={this.state.visibleNotificationModal} backdropOpacity={0.5}>
+                    <View style={modalstyles.modalContainer}>
+                        <Text style={modalstyles.headerTextStyle}>Notifications</Text>
+
+                        <View style={modalstyles.dropDownStyle}>
+                        <Dropdown choices={[
+                            {label: 'Friends', value: 'friends'},
+                            {label: 'Just me', value: 'just me'},
+                            {label: 'Turned off', value: 'turned off'},
+                        ]}/>
+                        </View>
+
+                        <TouchableOpacity style={modalstyles.buttonContainer} onPress={this.toggleNotificationModal}>
+                            <Text style={modalstyles.buttonText}>Okay</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
+
+                <Modal isVisible={this.state.visibleDatesModal} backdropOpacity={0.5}>
+                    <View style={modalstyles.modalContainer}>
+                        <Text style={modalstyles.headerTextStyle}>Date(s) of Trip</Text>
+
+                        <TouchableOpacity style={modalstyles.buttonContainer} onPress={this.toggleDatesModal}>
+                            <Text style={modalstyles.buttonText}>Okay</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
+
+            </View>
         );
     }
 }
@@ -105,7 +148,7 @@ const styles = StyleSheet.create({
         color: '#2661B2',
         fontWeight: 'bold',
     },
-////////////////Privacy Component///////////////////
+    ////////////////Privacy Component///////////////////
     privacyOptionContainer: {
         flexDirection: 'row',
         backgroundColor: '#f0f3f7',
@@ -113,6 +156,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         height: 60,
         marginBottom: 25,
+        marginTop: 25
     },
     privacyOptionText: {
         fontSize: 15,
@@ -121,7 +165,7 @@ const styles = StyleSheet.create({
         marginLeft: 30,
         marginRight: 130
     },
-////////////////Notifications, Manage Collaborators, Dates of Trip Component///////////////
+    ////////////////Notifications, Manage Collaborators, Dates of Trip Component///////////////
     buttonContainer: {
         flexDirection: 'row',
         backgroundColor: '#f0f3f7',
@@ -137,7 +181,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginLeft: 30
     },
-//////////////Delete Plan Component/////////////
+    //////////////Delete Plan Component/////////////
     deletePlanContainer: {
         backgroundColor: '#E23737',
         borderRadius: 30,
@@ -157,9 +201,49 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         paddingTop: 20
     },
-//////////////////Justifying options left & aligning/////////////////////
+    //////////////////Justifying options left & aligning/////////////////////
     organizeView: {
         flexDirection: 'row',
         marginLeft: 40
     },
+});
+
+/////////////////////////////////Modal Styling////////////////////////////////////
+const modalstyles = StyleSheet.create({
+    /////////////////////Button/////////////////////
+    buttonContainer: {
+        backgroundColor: '#Ed7248',
+        borderRadius: 30,
+        width: 270,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
+        marginTop: 20
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#ffffff',
+        fontSize: 11,
+        fontWeight: 'bold',
+    },
+    ////////////////////////Header////////////////////
+    headerTextStyle: {
+        fontSize: 30,
+        color: '#605985',
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 20
+    },
+    //////////////////////Modal Container//////////////////
+    modalContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    ////////////////////Drop Down/////////////////////////
+    dropDownStyle: {
+        width: 250,
+    }
 });
