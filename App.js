ba42acd0ecedf7e4635e9d4abb44bdf8d8e5f2b8
eshackleton13@apu.edu <eshackleton13@apu.edu>
2019-04-01 17:21:56 -0700

    Notifications button in Profile Settings now navigates to Notifications screen

--- App.js
+++ App.js
@@ -79,7 +79,8 @@ export default class App extends React.Component {
       {
         Profile: ProfileScreen,
         Settings: SettingsScreen,
-        MyPlans: MyPlansScreen
+        MyPlans: MyPlansScreen,
+        Notifications: NotificationsScreen
       }
     );
     const AppNavigator = createBottomTabNavigator(
